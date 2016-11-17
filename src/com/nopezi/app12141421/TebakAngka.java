88fb5daadea37b699b7c5aa212f6da55a1c435e6
nopezi <>
2016-11-17 14:23:10 +0700

    12141421 baris 17 = digunakan untuk meminta nilai inputan dari user pada saat program dijalankan
    12141421 baris 19 = variable dengan nama angka yang berisi nilai angka 
    yang harus ditebak
    
    12141421 baris 21 = menampilkan nilai yang berbentuk tulisan / string
    
    12141421 baris 22 = tempat menampung nilai input angka tebakan dari user

---
+++ src/com/nopezi/app12141421/TebakAngka.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nopezi.app12141421;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Administrator
+ */
+public class TebakAngka {
+    
+    public static void main(String[] args){
+        Scanner inputAngka = new Scanner(System.in);
+        
+        int angka = 10;
+        
+        System.out.println("masukkan angka yang ingin di tebak! ");
+        int tebakan = inputAngka.nextInt();
+    }
+    
+}
